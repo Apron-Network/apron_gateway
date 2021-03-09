c9966d250977e6cc892df26f4d2c026ad2e09bf3
kuleleyokn42 <kuleleyokn@protonmail.com>
2021-03-09 15:01:32 +0800

    Allow gateway to get configurations from env var

--- cmd/gw/main.go
+++ cmd/gw/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"apron.network/gateway/internal"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -17,6 +18,14 @@ import (
 	"apron.network/gateway/internal/handlers/ratelimiter"
 	"apron.network/gateway/internal/models"
 )
+
+func getEnv(key, defaultVal string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	} else {
+		return defaultVal
+	}
+}
 
 func startAdminService(addr string, wg *sync.WaitGroup, redisClient *redis.Client, manager models.AggregatedAccessRecordManager) {
 	h := handlers.ManagerHandler{
@@ -70,26 +79,24 @@ func startProxyService(addr string, wg *sync.WaitGroup, redisClient *redis.Clien
 }
 
 func main() {
-	// TODO: Add cli params support, to pass in config file path
 	// TODO: Define config file format - After logic finalized
 	wg := new(sync.WaitGroup)
 	wg.Add(2)
 
-	// CLI parser
-	redisServerPtr := flag.String("redis", "localhost:6379", "redis server used for saving service and user key")
-	proxyPortPtr := flag.Int("proxy-port", 8080, "proxy server port")
-	adminAddrPtr := flag.String("admin-addr", "127.0.0.1:8082", "Admin service address")
-	flag.Parse()
+	proxyPort, err := strconv.ParseInt(getEnv("PROXY_PORT", "8080"), 10, 32)
+	internal.CheckError(err)
+	adminAddrStr := getEnv("ADMIN_ADDR", "127.0.0.1:8082")
+	redisServer := getEnv("REDIS_SERVER", "localhost:6379")
 
-	proxyServerAddr := fmt.Sprintf(":%d", *proxyPortPtr)
+	proxyServerAddr := fmt.Sprintf(":%d", proxyPort)
 
 	fmt.Println("Service info:")
 	fmt.Printf("\tProxy addr: %s\n", proxyServerAddr)
-	fmt.Printf("\tAdmin service addr: %s\n", *adminAddrPtr)
-	fmt.Printf("\tRedis server: %s\n", *redisServerPtr)
+	fmt.Printf("\tAdmin service addr: %s\n", adminAddrStr)
+	fmt.Printf("\tRedis server: %s\n", redisServer)
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     *redisServerPtr,
+		Addr:     redisServer,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
@@ -98,7 +105,7 @@ func main() {
 	aggrAccessRecordManager.Init()
 
 	go startProxyService(proxyServerAddr, wg, rdb, aggrAccessRecordManager)
-	go startAdminService(*adminAddrPtr, wg, rdb, aggrAccessRecordManager)
+	go startAdminService(adminAddrStr, wg, rdb, aggrAccessRecordManager)
 
 	wg.Wait()
 }
